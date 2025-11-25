8973ab8799f9397b5a6296d8e4a9b220554b95a0
quest82 <awoniyioluseyi@gmail.com>
2025-11-25 16:31:41 +0100

    Correction completed Fixed the playGame() by streamlining the code

--- index.js
+++ index.js
@@ -1,63 +1,45 @@
 let humanScore = 0;
 let computerScore = 0;
 
-const humanSelection = getHumanChoice(); // Saves the result of the getHumanChoice function
-const computerSelection = getComputerChoice(); // Saves the result of the getComputerChoice function
+function playGame() {
+	for (let i = 1; i <= 5; i++) {
+		const humanSelection = getHumanChoice(); // Saves the result of the getHumanChoice function
+		const computerSelection = getComputerChoice(); // Saves the result of the getComputerChoice function
+
+		playRound(humanSelection, computerSelection);
+	}
+	console.log('Your final score is: ', humanScore);
+}
 
 function playRound(humanChoice, computerChoice) {
 	if (humanChoice === computerChoice) {
 		console.log("Same choice, let's play again");
 	} else if (humanChoice === "rock" && computerChoice === "paper") {
-		console.log(`Computer's choice was ${computerChoice}`);
 		console.log("You lose! Paper beats Rock");
 		computerScore += 1;
 	} else if (humanChoice === "scissors" && computerChoice === "rock") {
-		console.log(`Computer's choice was ${computerChoice}`);
 		console.log("You lose! Rock beats Scissors");
 		computerScore += 1;
 	} else if (humanChoice === "paper" && computerChoice === "scissors") {
-		console.log(`Computer's choice was ${computerChoice}`);
 		console.log("You lose! Scissors beats Paper");
 		computerScore += 1;
 	} else {
-		console.log(`Computer's choice was ${computerChoice}`);
 		console.log("You win!!");
 		humanScore += 1;
-        console.log(humanScore);
 	}
-}
+} // This holds the logic for each round of the game
 
 function getComputerChoice() {
 	let gamesArray = ["rock", "paper", "scissors"];
 	let randomChoice = Math.floor(Math.random() * gamesArray.length);
 	return gamesArray[randomChoice];
-}
+}  // This function randomly selects for the computer's choice
 
 function getHumanChoice() {
 	let choice = prompt(
 		"Enter your game choice between (rock, paper & scissors):"
 	);
 	return choice.toLowerCase();
-}
+} // This function asks for the choice of the player and makes it lower case
 
-// const playGame = function () {
-
-// 	for (let i = 1; i <= 5; i++) {
-//         const getComputerChoice = function () {
-//             let gamesArray = ["rock", "paper", "scissors"];
-//             let randomChoice = Math.floor(Math.random() * gamesArray.length);
-//             return gamesArray[randomChoice];
-//         }; // This function randomly selects the computer's choice between
-
-//         const getHumanChoice = function () {
-//             let choice = prompt("Enter your game choice between (rock, paper & scissors):");
-//             return choice;
-//         }; // This function asks for the choice of the player
-
-//         console.log(playRound(humanSelection, computerSelection))
-//         console.log(`Your score is ${humanScore}`);
-//         console.log(`Computer's score is ${computerScore}`);
-// 	}
-// };
-
-// console.log(playGame());
+playGame(); // Calls the playGame function
